0c4c092b94fff263ddd3039398180edd5b36866d
Hiếu JB <trunghieuit061099@gmail.com>
2021-07-06 13:47:59 +0700

    update view login registration and process bar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <router-view/>
+<div id="app">
+    <router-view />
+    <vue-progress-bar></vue-progress-bar>
+</div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    // HelloWorld
-  }
+    name: 'App',
+    components: {
+
+    },
+    mounted() {
+        this.$Progress.finish();
+    },
+    created() {
+        this.$Progress.start();
+        this.$router.beforeEach((to, from, next) => {
+            if (to.meta.progress !== undefined) {
+                let meta = to.meta.progress;
+                this.$Progress.parseMeta(meta);
+            }
+            this.$Progress.start();
+            next();
+        });
+        this.$router.afterEach(() => {
+            this.$Progress.finish();
+        });
+    },
 }
 </script>
 
--- src/router.js
+++ src/router.js
@@ -1,10 +1,30 @@
 import { createRouter,createWebHistory } from "vue-router";
-import Home from "./pages/home";
-
+import Login from "./pages/Login";
+import Registraion from "./pages/Registration"
+const meta = {
+    progress: {
+      func: [
+        { call: "color", modifier: "temp", argument: "#00c8ff" },
+        { call: "fail", modifier: "temp", argument: "#6e0000" },
+        { call: "location", modifier: "temp", argument: "top" },
+        {
+          call: "transition",
+          modifier: "temp",
+          argument: { speed: "1.5s", opacity: "0.6s", termination: 400 },
+        },
+      ],
+    },
+};
 const routes=[
     {
         path:"/",
-        component:Home,
+        component:Login,
+        meta: meta
+    },
+    {
+        path:"/registration",
+        component:Registraion,
+        meta: meta
     },
 ];
 
